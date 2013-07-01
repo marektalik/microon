5349468fbd7fef41160af96fc63a7da043bb4c83
Henryk Konsek <hekonsek@gmail.com>
2013-07-01 16:21:37 +0200

    Added Scaladoc.

--- microon-services/microon-services-auth/src/main/scala/microon/services/auth/AuthProvider.scala
+++ microon-services/microon-services-auth/src/main/scala/microon/services/auth/AuthProvider.scala
@@ -1,7 +1,17 @@
 package microon.services.auth
 
+/**
+ * Provides handler that could be used against the [[microon.services.auth.AuthRequest]] instance in
+ * order to validate the user credentials.
+ */
 trait AuthProvider {
 
+  /**
+   * Return authentication handler.
+   *
+   * @return Partial function capable of handling the [[microon.services.auth.AuthRequest]] and invoking procedure
+   *         of verification of the user credentials.
+   */
   def authenticator: PartialFunction[AuthRequest, Unit]
 
 }
