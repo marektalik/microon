34197eaef46ec4485a268b127a0e98b7b9f45c4e
Henryk Konsek <hekonsek@gmail.com>
2013-07-22 14:56:04 +0200

    Added Repository Service tests.

---
+++ microon-services/microon-services-repository/src/test/scala/microon/services/repository/impl/DefaultRepositoryServiceTest.scala
@@ -0,0 +1,59 @@
+package microon.services.repository.impl
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.springframework.scala.context.function.{ContextSupport, FunctionalConfiguration}
+import microon.ri.boot.spring.scala.SpringScalaBoot
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.mockito.Mockito._
+import microon.services.repository.api.scala.RepositoryService
+
+class DefaultRepositoryServiceTest extends FunSuite with BeforeAndAfter {
+
+  val id = "id"
+  val foo = Foo(id)
+
+  val boot = SpringScalaBoot[TestConfig].start()
+  val service = boot.context[RepositoryService[Foo, String]]
+  val repository = boot.context[PagingAndSortingRepository[Foo, String]]
+
+  before {
+    reset(repository)
+  }
+
+  test("Should save entity.") {
+    service.save(foo).get
+    verify(repository).save(foo)
+  }
+
+  test("Should save entities.") {
+    val entities = Seq(foo, foo)
+    service.save(entities).get
+    verify(repository).save(entities.toList)
+  }
+
+  test("Should find entity.") {
+    service.findOne(id).get
+    verify(repository).findOne(id)
+  }
+
+  test("Should verify entity existence.") {
+    service.exists(id).get
+    verify(repository).exists(id)
+  }
+
+}
+
+class TestConfig extends FunctionalConfiguration with ContextSupport {
+  enableAnnotationConfig()
+
+  val repository = bean() {
+    mock(classOf[PagingAndSortingRepository[Foo, String]], RETURNS_DEEP_STUBS)
+  }
+
+  bean() {
+    new DefaultRepositoryService(repository())
+  }
+
+}
+
+case class Foo(id: String)
