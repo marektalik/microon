b272e01fd7a322353bf7c6af6aeabb4977059222
Henryk Konsek <hekonsek@gmail.com>
2013-09-13 07:45:59 +0200

    Added Scaladoc.

--- microon-services/microon-services-validator/src/main/scala/microon/services/validator/api/scala/ValidatorService.scala
+++ microon-services/microon-services-validator/src/main/scala/microon/services/validator/api/scala/ValidatorService.scala
@@ -2,6 +2,9 @@ package microon.services.validator.api.scala
 
 import java.util.concurrent.Future
 
+/**
+ * Service dedicated for checking the state correctness and constrains against the business data.
+ */
 trait ValidatorService {
 
   def validate(subject: Any): Future[Option[Array[String]]]
