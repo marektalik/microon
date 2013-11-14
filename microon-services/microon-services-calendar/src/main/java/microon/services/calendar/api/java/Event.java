f806aef4bd317c6272e0032fe938a1b0a36b38cf
Henryk Konsek <hekonsek@gmail.com>
2013-11-14 18:32:07 +0100

    Added Google  Calendar eventId to Java wrapper event object.

--- microon-services/microon-services-calendar/src/main/java/microon/services/calendar/api/java/Event.java
+++ microon-services/microon-services-calendar/src/main/java/microon/services/calendar/api/java/Event.java
@@ -4,16 +4,21 @@ import java.util.Date;
 
 public class Event {
 
+    private final String eventId;
     private final String summary;
     private final Date from;
     private final String[] attendees;
 
-    public Event(String summary, Date from, String[] attendees) {
+    public Event(String eventId, String summary, Date from, String[] attendees) {
         this.summary = summary;
         this.from = from;
         this.attendees = attendees;
+        this.eventId = eventId;
     }
 
+    public String eventId() {
+        return eventId;
+    }
 
     public String summary() {
         return summary;
--- microon-services/microon-services-calendar/src/main/scala/microon/services/calendar/google/CamelGoogleCalendarServiceJavaWrapper.scala
+++ microon-services/microon-services-calendar/src/main/scala/microon/services/calendar/google/CamelGoogleCalendarServiceJavaWrapper.scala
@@ -5,11 +5,10 @@ import java.util.Date
 import java.util.concurrent.Future
 import microon.services.calendar.api.scala.CalendarService
 import com.google.common.util.concurrent.Futures.immediateFuture
-import scala.collection.JavaConversions._
 
 class CamelGoogleCalendarServiceJavaWrapper(calendarService: CalendarService) extends JavaCalendarService {
 
   def listEvents(from: Date): Future[Array[JavaEvent]] =
-    immediateFuture(calendarService.listEvents(Option(from)).get().map(event => new JavaEvent(event.summary, event.from, event.attendees.toArray)).toArray)
+    immediateFuture(calendarService.listEvents(Option(from)).get().map(event => new JavaEvent(event.eventId, event.summary, event.from, event.attendees.toArray)).toArray)
 
 }
